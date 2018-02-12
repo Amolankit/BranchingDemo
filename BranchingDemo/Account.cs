a08120fe32a727b1ab7b0adc5d13420b2951ca36
Amol Ankit <amolankit@live.com>
2018-02-12 23:32:20 +0530

    Add project files.

---
+++ BranchingDemo/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BranchingDemo
+{
+    class Account
+    {
+        public decimal Balance { get; private set; }
+
+        private IAccountState State { get; set; }
+
+        public Account(Action onUnfreeze)
+        {
+            State = new NotVerified(onUnfreeze);
+        }
+
+        // #1 (Interaction): Deposit was invoked on the State
+        // #2 (Behavior): Result of State.Deposit is new State
+        // #5 (Behavior): Deposit 10, Deposit 1 - Balance == 11
+        public void Deposit(decimal amount)
+        {
+            State = State.Deposit(() => { Balance += amount; });
+        }
+
+        // #3 (Interaction): Withdraw was invoked on the State
+        // #4 (Behavior): Result of State.Withdraw is new State
+        // #6 (Behavior): Deposit 1, Verify, Withdraw 1 - Balance == 9
+        public void Withdraw(decimal amount)
+        {
+            State = State.Withdraw(() => { Balance -= amount; });
+        }
+
+        public void HolderVerified()
+        {
+            State = State.HolderVerified();
+        }
+
+        public void Close()
+        {
+            State = State.Close();
+        }
+
+        public void Freeze()
+        {
+            State = State.Freeze();
+        }
+    }
+}
